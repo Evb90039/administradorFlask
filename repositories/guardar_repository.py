480f5a6998d863ff08c9ebdff94e2a60a8710f8d
Enrique Vizcarra Beltran <enrique.vizcarra@aforecoppel.com>
2025-05-16 14:22:30 -0700

    Prueba de log

--- repositories/guardar_repository.py
+++ repositories/guardar_repository.py
@@ -1,8 +1,10 @@
 """Repositorio para operaciones de guardado en Firestore."""
+import logging
 from config.firebase import db
 
 def guardar(guardar_data):
     """Guarda los datos en la colección 'pruebaback' de Firestore."""
+    logging.info("Entrando a la función guardar con data: %s", guardar_data)
     doc_ref = db.collection('pruebaback').add({
         'nombre': guardar_data.nombre,
         'apellido': guardar_data.apellido,
