38b6f19d1972caf12a4f3c8931bafc3271e541a8
Enrique Vizcarra Beltran <enrique.vizcarra@aforecoppel.com>
2025-05-16 13:54:29 -0700

    Se sube

--- routes/guardar.py
+++ routes/guardar.py
@@ -3,8 +3,9 @@ from flask import Blueprint, request
 from controllers.guardar_controller import guardar
 
 guardar_bp = Blueprint('guardar', __name__, url_prefix='/guardar')
+guardar_bp.add_url_rule('/', view_func=guardar, methods=['POST'])
 
 @guardar_bp.route('/', methods=['POST'])
 def guardar_route():
     """Endpoint para guardar datos mediante POST."""
-    return guardar(request)
+    return guardar(request.get_json(force=True))
