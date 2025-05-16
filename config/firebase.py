4be3b3c2ccf878d2dc032516195eadc2e16c70ba
Enrique Vizcarra Beltran <enrique.vizcarra@aforecoppel.com>
2025-05-16 13:24:23 -0700

    Se ssube credenciales

--- config/firebase.py
+++ config/firebase.py
@@ -4,9 +4,17 @@ from firebase_admin import credentials, firestore
 
 # Cargar credenciales desde variables de entorno
 cred = credentials.Certificate({
-    "projectId": os.environ.get("FIREBASE_PROJECT_ID"),
-    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
-    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "type": os.environ.get("type"),
+    "project_id": os.environ.get("project_id"),
+    "private_key_id": os.environ.get("private_key_id"),
+    "private_key": os.environ.get("private_key").replace('\\n', '\n'),
+    "client_email": os.environ.get("client_email"),
+    "client_id": os.environ.get("client_id"),
+    "auth_uri": os.environ.get("auth_uri"),
+    "token_uri": os.environ.get("token_uri"),
+    "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+    "client_x509_cert_url": os.environ.get("client_x509_cert_url"),
+    "universe_domain": os.environ.get("universe_domain"),
 })
 
 firebase_admin.initialize_app(cred)
